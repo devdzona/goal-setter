37e60f28f0381ef1a08bb6dc453e69bec4afb400
Nikola Ostojic <dzonaberlin@gmail.com>
2023-06-29 15:08:24 +0200

    Initial components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
+    <>
+    <Router>
+    <div className='container'>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
+    </Router>
+    </>
   );
 }
 
