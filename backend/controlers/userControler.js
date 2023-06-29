610c4b0f1872cc31cbd335e8101c1023891f78c2
Nikola Ostojic <dzonaberlin@gmail.com>
2023-06-29 17:34:52 +0200

    Small bug fix in backend in Controlers

--- backend/controlers/userControler.js
+++ backend/controlers/userControler.js
@@ -7,14 +7,14 @@ const User = require('../model/userModel')
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
 
-    if(!name || !email || !password) {
+    if (!name || !email || !password) {
         res.status(400)
         throw new Error('Please add all fields')
     }
 
     //Check if user exists
-    const userExists = await User.findOne({email})
-    if(userExists) {
+    const userExists = await User.findOne({ email })
+    if (userExists) {
         res.status(400)
         throw new Error('User already exists')
     }
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async (req, res) => {
         password: hashedPassword
     })
 
-    if(user) {
+    if (user) {
         res.status(201).json({
             _id: user.id,
             name: user.name,
@@ -45,9 +45,9 @@ const registerUser = asyncHandler(async (req, res) => {
 // Authenticate a user // POST /api/users // Public
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
-    const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
-    if(user && (await bcrypt.compare(password, user.password))) {
+    if (user && (await bcrypt.compare(password, user.password))) {
         res.json({
             _id: user.id,
             name: user.name,
@@ -62,18 +62,12 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // Get user data // GET /api/users/me // Private
 const getMe = asyncHandler(async (req, res) => {
-    const { _id, name, email } = await User.findById(req.user.id)
-
-    res.status(200).json({
-        id: _id,
-        name: name,
-        email: email
-    })
+    res.status(200).json(req.user)
 })
 
 //Generate JWT
 const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '30d'})
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' })
 }
 
 module.exports = {
