6c6e6aafbc250fa88b8bc3fbef24cce2cae289aa
Nikola Ostojic <dzonaberlin@gmail.com>
2023-06-29 16:30:02 +0200

    Registration FE logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header';
 
 function App() {
   return (
     <>
-    <Router>
-    <div className='container'>
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<Dashboard></Dashboard>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-      </Routes>
-    </div>
-    </Router>
+      <Router>
+        <div className='container'>
+          <Header></Header>
+          <Routes>
+            <Route path='/' element={<Dashboard></Dashboard>}></Route>
+            <Route path='/register' element={<Register></Register>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer></ToastContainer>
     </>
   );
 }
